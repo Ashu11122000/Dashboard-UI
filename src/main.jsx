d2dd673f2acae47542dc9c4779266d0edc5ecf25
Ashish Sharma <ashu11vats@gmail.com>
2026-05-18 16:07:02 +0530

    Rename the file ThemeContext.jsx in place of ThemeProvider.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import ThemeProvider from "./context/ThemeProvider";
+import ThemeProvider from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
